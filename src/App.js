157a8aba703b1780b527b81c26cc3af0c3ef53ba
Menchu MV <mcmvillalobos@gmail.com>
2021-11-01 16:33:19 +0100

    Added task component (only rendering)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Form from "./components/Form/Form";
+import Task from "./components/Task";
 
 function App() {
   return (
     <div className="todo-app container">
-      <h1 className="todo-app__title">Do things</h1>
+      <h1 className="todo-app__title">Do things!!!!</h1>
       <Form />
+      <Task />
     </div>
   );
 }
