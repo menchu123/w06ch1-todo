e83df7d35e5b200ec3d6d5059b87dc82f4ac6f3d
Menchu MV <mcmvillalobos@gmail.com>
2021-11-01 21:04:58 +0100

    Added provider to Form tests

--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -1,10 +1,17 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import configureStore from "../../redux/store";
 import Form from "./Form";
 
 describe("Given a Form component", () => {
   describe("When it is called", () => {
     test("Then it should render an input text", () => {
-      render(<Form />);
+      const store = configureStore();
+      render(
+        <Provider store={store}>
+          <Form />
+        </Provider>
+      );
 
       const textbox = screen.getByRole("textbox");
 
@@ -12,7 +19,12 @@ describe("Given a Form component", () => {
     });
 
     test("Then it should render a submit button", () => {
-      render(<Form />);
+      const store = configureStore();
+      render(
+        <Provider store={store}>
+          <Form />
+        </Provider>
+      );
 
       const button = screen.getByRole("button");
 
