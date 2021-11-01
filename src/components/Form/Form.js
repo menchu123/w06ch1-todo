94b74ecbdb770f50b2999682680a4096921b44c2
Menchu MV <mcmvillalobos@gmail.com>
2021-11-01 20:38:30 +0100

    Added onChange function to value in input

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,16 +1,30 @@
+import { useState } from "react";
+import useTasks from "../../hooks/useTasks";
+
 const Form = () => {
+  const { tasks, createTask } = useTasks();
+
+  const initialData = { task: "" };
+  const [taskData, setTaskData] = useState(initialData);
+
+  const changeData = (event) => {
+    setTaskData({ ...taskData, [event.target.id]: event.target.value });
+  };
+
   return (
     <div className="form-row align-items-center todo__form">
       <div className="row">
         <div className="form-group col-10">
           <input
             type="text"
-            id="task-input"
+            id="task"
             className="form-control"
             required
             formNoValidate
             autoComplete="off"
             placeholder="What do you want to do?"
+            value={taskData.task}
+            onChange={changeData}
           ></input>
         </div>
         <div className="form-group col-2 p-0">
