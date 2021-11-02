802a4fba96238f0d22012755364d23260b252c61
Menchu MV <mcmvillalobos@gmail.com>
2021-11-02 19:05:25 +0100

    Added condition for Edit button

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -13,13 +13,15 @@ const Task = ({ task }) => {
   const initialData = task.task;
 
   const [taskData, setTaskData] = useState(initialData);
+  const [isEditing, setisEditing] = useState(false);
 
   const onChange = (event) => {
     event.preventDefault();
     setTaskData(event.target.value);
+    setisEditing(true);
   };
 
-  const onSubmit = (event) => {
+  const onUpdate = (event) => {
     event.preventDefault();
     const newTask = {
       id: task.id,
@@ -28,6 +30,7 @@ const Task = ({ task }) => {
     };
 
     updateTask(newTask);
+    setisEditing(false);
     event.target.querySelector(".task__text").blur();
   };
 
@@ -40,21 +43,28 @@ const Task = ({ task }) => {
           id="task"
           className="form-check-input"
         />
-        <form onSubmit={(event) => onSubmit(event)}>
-          <input
-            type="text"
-            className="task__text form-control border-0"
-            value={taskData}
-            onChange={(event) => onChange(event)}
-          ></input>
+        <form
+          className="form d-flex justify-content-between"
+          onSubmit={isEditing ? (event) => onUpdate(event) : onDelete}
+        >
+          <div className="form-group task__text-input">
+            <input
+              type="text"
+              className="task__text form-control border-0"
+              value={taskData}
+              onChange={(event) => onChange(event)}
+              onClick={(event) => onChange(event)}
+            ></input>
+          </div>
+          <button
+            className={`task__delete btn btn-${
+              isEditing ? "primary" : "danger"
+            } pull-right`}
+          >
+            {isEditing ? "Edit" : "-"}
+          </button>
         </form>
       </div>
-      <button
-        className="task__delete btn btn-danger pull-right"
-        onClick={onDelete}
-      >
-        -
-      </button>
     </li>
   );
 };
